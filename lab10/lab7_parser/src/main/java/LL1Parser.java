22722bfbcb28f45582cf31ac372f52df83babb49
SerbanIoana <72523587+SerbanIoana@users.noreply.github.com>
2021-12-03 15:09:39 +0200

    Add files via upload

---
+++ lab10/lab7_parser/src/main/java/LL1Parser.java
@@ -0,0 +1,243 @@
+import java.util.*;
+
+public class LL1Parser {
+    private final String EPSILON = "eps";
+    private Grammar grammar;
+    private Map<String, List<String>> first;
+    private Map<String, List<String>> follow;
+    private List<ParseTableCell> parseTable;
+//    private Stack<String> inputStack;
+//    private Stack<String> workingStack;
+//    private List<String> output;
+
+
+    public LL1Parser(Grammar grammar) {
+        this.grammar = grammar;
+        this.first = new LinkedHashMap<>();
+        this.follow = new LinkedHashMap<>();
+        constructFirst();
+        constructFollow();
+        this.parseTable = new ArrayList<>();
+        //constructParseTable();
+//        this.inputStack = new Stack<>();
+//        this.workingStack = new Stack<>();
+//        this.output = new ArrayList<>();
+    }
+
+    private void constructParseTable() {
+        //add entry ($,$)=acc
+        List<String> tableProduction = new ArrayList<>();
+        tableProduction.add("acc");
+        this.parseTable.add(new ParseTableCell("$", "$", tableProduction, 0));
+
+        List<String> terminals = grammar.getTerminals();
+        List<String> nonterminals = grammar.getNonTerminals();
+
+        //add entries (terminal,terminal)=pop
+        for (String terminal : terminals) {
+            tableProduction = new ArrayList<>();
+            tableProduction.add("pop");
+            ParseTableCell parseTableCell = new ParseTableCell(terminal, terminal, tableProduction, -1);
+            this.parseTable.add(parseTableCell);
+        }
+
+        //add entries for (nonterminal,terminal)=(production,productionIndex)
+        for (String nonterminal : nonterminals) {
+            for (Production production : grammar.getProductionsForNonterminal(nonterminal)) {
+                List<String> firstOfProduction = getFirstOfProduction(production.getRule(), this.first);
+                tableProduction = production.getRule();
+                int productionIndex = grammar.getProductions().indexOf(production);
+
+                if (!firstOfProduction.contains(EPSILON)) {
+                    for (String terminal : firstOfProduction) {
+                        if (!this.first.get(nonterminal).contains(terminal))
+                            break;
+                        ParseTableCell parseTableCell = new ParseTableCell(nonterminal, terminal, tableProduction, productionIndex);
+                        this.parseTable.add(parseTableCell);
+                    }
+                }
+                else {
+                    for (String terminal : this.follow.get(nonterminal)) {
+                        ParseTableCell parseTableCell;
+                        if (terminal.equals(EPSILON))
+                            parseTableCell = new ParseTableCell(nonterminal, "$", tableProduction, productionIndex);
+                        else
+                            parseTableCell = new ParseTableCell(nonterminal, terminal, tableProduction, productionIndex);
+                        this.parseTable.add(parseTableCell);
+                    }
+                }
+            }
+        }
+    }
+
+    private List<String> getFirstOfProduction(List<String> rule, Map<String, List<String>> first_map) {
+        List<String> productionFirsts = new ArrayList();
+        boolean epsilonFound = true;
+        int i = 0;
+
+        while (i < rule.size()) {
+            String symbol = rule.get(i);
+            List<String> symbolFirsts = first_map.get(symbol);
+
+            productionFirsts.addAll(symbolFirsts);
+            if (!symbolFirsts.contains(EPSILON)) {
+                epsilonFound = false;
+                break;
+            }
+            productionFirsts.remove(EPSILON);
+            i++;
+        }
+        //if epsilon is in all firsts, add it
+        if (epsilonFound)
+            productionFirsts.add(EPSILON);
+
+        return productionFirsts;
+    }
+
+    public void constructFirst() {
+        //init terminals f0
+        Map<String, List<String>> first_aux = new LinkedHashMap<>();
+        for (String terminal : grammar.getTerminals()) {
+            first_aux.put(terminal, new ArrayList<>());
+            first_aux.get(terminal).add(terminal);
+        }
+
+        //init nonterminals f0
+        for (String nonTerminal : grammar.getNonTerminals()) {
+            first_aux.put(nonTerminal, new ArrayList<>());
+            List<Production> productionsNonterminal = grammar.getProductionsForNonterminal(nonTerminal);
+            for (Production production : productionsNonterminal) {
+                String firstSymbol = production.getRule().get(0);
+                if (firstSymbol.equals(EPSILON) || grammar.getTerminals().contains(firstSymbol) && !first_aux.get(nonTerminal).contains(firstSymbol))
+                    first_aux.get(nonTerminal).add(firstSymbol);
+            }
+        }
+
+        boolean doneFirst = false;
+        Map<String, List<String>> first_current = new LinkedHashMap<>();
+
+        //starting with f1
+        while (!doneFirst) {
+            for (String terminal : grammar.getTerminals()) {
+                first_current.put(terminal, new ArrayList<>());
+                first_current.get(terminal).addAll(first_aux.get(terminal));
+            }
+
+            for (String nonTerminal : grammar.getNonTerminals()) {
+                //copy all previous
+                first_current.put(nonTerminal, new ArrayList<>());
+                first_current.get(nonTerminal).addAll(first_aux.get(nonTerminal));
+
+                for (Production production : grammar.getProductionsForNonterminal(nonTerminal)) {
+                    List<String> rule = production.getRule();
+
+                    //continue only if rule doesn't start with terminal
+                    if(!grammar.getTerminals().contains(rule.get(0)) && !rule.get(0).equals(EPSILON)) {
+                        //check if first is previously computed for all symbols of rule
+                        boolean canCompute = true;
+                        for (String symbol : rule) {
+                            List<String> symbolFirsts = first_aux.get(symbol);
+                            if (symbolFirsts.isEmpty()) {
+                                canCompute = false;
+                                break;
+                            }
+                        }
+
+                        if (canCompute) {
+                            List<String> productionFirsts = getFirstOfProduction(rule, first_aux);
+
+                            for (String terminal : productionFirsts) {
+                                if (!first_current.get(nonTerminal).contains(terminal))
+                                    first_current.get(nonTerminal).add(terminal);
+                            }
+                        }
+                    }
+                }
+            }
+
+            doneFirst = first_current.entrySet()
+                    .stream()
+                    .allMatch(e -> Arrays.equals(new List[]{e.getValue()}, new List[]{first_aux.get(e.getKey())}));
+
+            for (String key : first_aux.keySet()) {
+                first_aux.get(key).clear();
+                first_aux.get(key).addAll(first_current.get(key));
+            }
+            first_current.clear();
+        }
+        this.first = first_aux;
+    }
+
+    public void constructFollow() {
+        Map<String, List<String>> follow_aux = new LinkedHashMap<>();
+        //init nonterminals l0
+        for (String nonTerminal : grammar.getNonTerminals()) {
+            follow_aux.put(nonTerminal, new ArrayList<>());
+        }
+        follow_aux.get(grammar.getStartSymbol()).add(EPSILON);
+
+        boolean doneFollow = false;
+        Map<String, List<String>> follow_current = new LinkedHashMap<>();
+
+        //starting with l1
+        while (!doneFollow) {
+            for (String nonTerminal : grammar.getNonTerminals()) {
+                follow_current.put(nonTerminal, new ArrayList<>());
+                follow_current.get(nonTerminal).addAll(follow_aux.get(nonTerminal));
+
+                for (Production production : grammar.getProductionsContainingNonterminal(nonTerminal)) {
+                    List<String> rule = production.getRule();
+                    String nonterminalStart = production.getNonTerminal();
+                    List<String> productionFollow = new ArrayList();
+                    boolean epsilonFound = true;
+                    int i = rule.indexOf(nonTerminal) + 1;
+
+                    while (i < rule.size()) {
+                        String symbol = rule.get(i);
+                        List<String> symbolFirsts = this.first.get(symbol);
+
+                        productionFollow.addAll(symbolFirsts);
+                        if (!symbolFirsts.contains(EPSILON)) {
+                            epsilonFound = false;
+                            break;
+                        }
+                        productionFollow.remove(EPSILON);
+                        i++;
+                    }
+
+                    //if epsilon is in all firsts, add follow of the start nonterminal for production (from previous step)
+                    if (epsilonFound)
+                        productionFollow.addAll(follow_aux.get(nonterminalStart));
+
+                    for (String terminal : productionFollow) {
+                        if (!follow_current.get(nonTerminal).contains(terminal))
+                            follow_current.get(nonTerminal).add(terminal);
+                    }
+                }
+            }
+
+            doneFollow = follow_current.entrySet()
+                    .stream()
+                    .allMatch(e -> Arrays.equals(new List[]{e.getValue()}, new List[]{follow_aux.get(e.getKey())}));
+
+            for (String key : follow_aux.keySet()) {
+                follow_aux.get(key).clear();
+                follow_aux.get(key).addAll(follow_current.get(key));
+            }
+            follow_current.clear();
+        }
+        this.follow = follow_aux;
+    }
+
+    public Grammar getGrammar() {
+        return grammar;
+    }
+
+    public Map<String, List<String>> getFirst() {
+        return first;
+    }
+
+    public Map<String, List<String>> getFollow() {
+        return follow;
+    }
+}
---
+++ lab10/lab7_parser/src/main/java/Main.java
@@ -0,0 +1,200 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    private static final Grammar grammar = new Grammar("g4.txt");
+    private static final LL1Parser parser = new LL1Parser(grammar);
+
+    public static void main(String[] args) {
+        //runGrammarMenu();
+        runParserMenu();
+    }
+
+    private static void printGrammarMenu() {
+        System.out.println("\n_____Menu_____");
+        System.out.println("1. Display the set of nonterminals");
+        System.out.println("2. Display the start symbol");
+        System.out.println("3. Display the set of terminals");
+        System.out.println("4. Display the set of productions");
+        System.out.println("5. Display productions for a given nonterminal");
+        System.out.println("6. Check if grammar is context-free");
+        System.out.println("0. EXIT");
+        System.out.print(">>>");
+    }
+
+    private static void printParserMenu() {
+        System.out.println("\n_____Menu_____");
+        System.out.println("1. Display FIRST");
+        System.out.println("2. Display FOLLOW");
+        System.out.println("0. EXIT");
+        System.out.print(">>>");
+    }
+
+    private static void runGrammarMenu() {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            printGrammarMenu();
+            try {
+                int command = input.nextInt();
+                switch (command) {
+                    case 0:
+                        System.exit(0);
+                    case 1:
+                        printNonterminals();
+                        break;
+                    case 2:
+                        printStartSymbol();
+                        break;
+                    case 3:
+                        printTerminals();
+                        break;
+                    case 4:
+                        printAllProductions();
+                        break;
+                    case 5:
+                        printProductionsForNonterminal();
+                        break;
+                    case 6:
+                        checkCFG();
+                        break;
+                    default:
+                        System.out.println("Invalid Command");
+                        break;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    private static void runParserMenu() {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            printParserMenu();
+            try {
+                int command = input.nextInt();
+                switch (command) {
+                    case 0:
+                        System.exit(0);
+                    case 1:
+                        printFirst();
+                        break;
+                    case 2:
+                        printFollow();
+                        break;
+                    default:
+                        System.out.println("Invalid Command");
+                        break;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    private static void printFollow() {
+        StringBuilder followStr = new StringBuilder("FOLLOW set\n");
+        for (Map.Entry<String, List<String>> followEntry : parser.getFollow().entrySet()) {
+            String symbol = followEntry.getKey();
+            List<String> symbolFollow = followEntry.getValue();
+            followStr.append("FOLLOW(").append(symbol).append(") = {");
+
+            for (String terminal : symbolFollow) {
+                followStr.append(terminal).append(", ");
+            }
+            followStr.delete(followStr.length() - 2, followStr.length());
+            followStr.append("}\n");
+        }
+        System.out.println(followStr);
+    }
+
+    private static void printFirst() {
+        StringBuilder firstStr = new StringBuilder("FIRST set\n");
+        for (Map.Entry<String, List<String>> firstEntry: parser.getFirst().entrySet()) {
+            String symbol = firstEntry.getKey();
+            List<String> symbolFirsts = firstEntry.getValue();
+            firstStr.append("FIRST(").append(symbol).append(") = {");
+
+            for (String terminal : symbolFirsts) {
+                firstStr.append(terminal).append(", ");
+            }
+            firstStr.delete(firstStr.length() - 2, firstStr.length());
+            firstStr.append("}\n");
+        }
+        System.out.println(firstStr);
+    }
+
+    private static void printNonterminals() {
+        StringBuilder nonterminalsStr = new StringBuilder("N = {");
+        for (String nonterminal : grammar.getNonTerminals()) {
+            nonterminalsStr.append(nonterminal).append(", ");
+        }
+        nonterminalsStr.delete(nonterminalsStr.length() - 2, nonterminalsStr.length());
+        nonterminalsStr.append("}");
+        System.out.println(nonterminalsStr);
+    }
+
+    private static void printStartSymbol() {
+        System.out.println("S = " + grammar.getStartSymbol());
+    }
+
+    private static void printTerminals() {
+        StringBuilder terminalsStr = new StringBuilder("T = {");
+        for (String terminal : grammar.getTerminals()) {
+            terminalsStr.append(terminal).append(", ");
+        }
+        terminalsStr.delete(terminalsStr.length() - 2, terminalsStr.length());
+        terminalsStr.append("}");
+        System.out.println(terminalsStr);
+    }
+
+    private static void printAllProductions() {
+        StringBuilder productionsStr = new StringBuilder("P = {\n");
+        for (Production production : grammar.getProductions()) {
+            productionsStr.append(production.toString()).append("\n");
+        }
+        productionsStr.delete(productionsStr.length() - 2, productionsStr.length());
+        productionsStr.append("\n}");
+        System.out.println(productionsStr);
+    }
+
+    private static void printProductionsForNonterminal() {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(">>nonterminal: ");
+        try {
+            String nonterminal = bufferedReader.readLine();
+            List<Production> productions = grammar.getProductionsForNonterminal(nonterminal);
+
+            if (productions == null)
+                System.out.println("Inexistent nonterminal!");
+            else {
+                StringBuilder productionsStr = new StringBuilder("Productions for nonterminal ");
+                productionsStr.append(nonterminal).append(" = {\n");
+                for (Production production : productions) {
+                    productionsStr.append(production.toString()).append(",\n");
+                }
+                productionsStr.delete(productionsStr.length() - 2, productionsStr.length());
+                productionsStr.append("\n}");
+                System.out.println(productionsStr);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void checkCFG() {
+        boolean accepted = grammar.checkContextFree();
+        if (accepted)
+            System.out.println("The given grammar is context-free");
+        else
+            System.out.println("The given grammar is NOT context-free");
+    }
+}
